08b99be5a5eba49ce0440f5c70580471858718b0
kenji0622s <kenji0622s@gmail.com>
2024-12-17 01:28:29 +0900

    fix

--- src/app/(main)/edit/[id]/page.tsx
+++ src/app/(main)/edit/[id]/page.tsx
@@ -9,14 +9,25 @@ interface Params {
 
 const getTask = async (id: string): Promise<TaskDocument> => {
   const response = await fetch(`${process.env.API_URL}/tasks/${id}`);
+  
+  // レスポンスの状態を確認
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+
   const data = await response.json();
+
+  // データが正しいか確認
+  if (!data || !data.task) {
+    throw new Error("Invalid data received");
+  }
+
   return data.task as TaskDocument;
 };
 
 const EditTaskPage = async ({ params }: Params) => {
-  const { id } = await params;
+  const { id } = params; // ここでawaitは不要
   const task = await getTask(id);
-  console.log(task);
   return (
     <div className="flex flex-col justify-center py-20">
       <h2 className="text-center text-2xl font-bold">Edit Task</h2>
